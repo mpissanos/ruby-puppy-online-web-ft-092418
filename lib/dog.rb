0882749edf92ddd368489960b375abe232a97756
Manny Pissanos <mpissano@gmail.com>
2018-10-01 18:21:48 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+  
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    # adding ( @@all << self ) below returns the dogs location in memory, not the actual names.
+    @@all << self
+  end
+  
+  def self.all
+    @@all.each { |x| puts x.name}
+  end
+  
+  def self.clear_all
+    @@all.clear
+  end
+end
